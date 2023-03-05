27b4cb8a2957b6ef60369ed9d8e373c66940ba45
Luca Dall'Olio <luca.dallolio@aego.ai>
2023-03-05 17:59:28 +0100

    cmake seems to work on linux

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,20 +1,25 @@
+REM Remove dot from PY_VER for use in library name
+set MY_PY_VER=%PY_VER:.=%
+
 mkdir build
 cd build
 
 cmake -G "Ninja" ^
       -D CMAKE_BUILD_TYPE=Release ^
-      -D CMAKE_PREFIX_PATH:FILEPATH=%PREFIX% ^
-      -D CMAKE_INSTALL_PREFIX:FILEPATH=%LIBRARY_PREFIX% ^
-      -D HDF5_ROOT_DIR:FILEPATH=%LIBRARY_PREFIX% ^
+      -D "CMAKE_PREFIX_PATH:FILEPATH=%PREFIX%" ^
+      -D "CMAKE_INSTALL_PREFIX:FILEPATH=%PREFIX%" ^
+      -D "HDF5_ROOT_DIR:FILEPATH=%LIBRARY_PREFIX%" ^
       -D MEDFILE_INSTALL_DOC=OFF ^
       -D MEDFILE_BUILD_PYTHON=ON ^
-      -D PYTHON_LIBRARY:FILEPATH=%PREFIX%\libs ^
-      -D PYTHON_INCLUDE_DIR:FILEPATH=%PREFIX%\include ^
-      ..
+      -D "PYTHON_EXECUTABLE:FILEPATH=%PYTHON%" ^
+      -D "PYTHON_INCLUDE_DIR:PATH=%PREFIX%\include" ^
+      -D "PYTHON_LIBRARY:FILEPATH=%PREFIX%\libs\python%MY_PY_VER%.lib" ^
+      -D PYTHON_INCLUDE_DIR:FILEPATH=%PREFIX%\include -S . -B build
 
 if errorlevel 1 exit 1
-ninja install
+cmake --build .\build --config Release
 if errorlevel 1 exit 1
+cmake --install .\build
 REM ninja test
 REM if errorlevel 1 exit 1
 
