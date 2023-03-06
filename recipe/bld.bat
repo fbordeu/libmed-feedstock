e85c44d120e1e512a62b6a34d9696e15de701dd2
Luca Dall'Olio <luca.dallolio@aego.ai>
2023-03-06 22:19:47 +0100

    adding xcopy for med library

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -2,13 +2,13 @@ REM Remove dot from PY_VER for use in library name
 set MY_PY_VER=%PY_VER:.=%
 
 cmake -G "Ninja" ^
+      -Wno-dev ^
       -D CMAKE_BUILD_TYPE=Release ^
       -D "CMAKE_PREFIX_PATH:FILEPATH=%PREFIX%" ^
       -D "CMAKE_INSTALL_PREFIX:FILEPATH=%PREFIX%" ^
       -D "HDF5_ROOT_DIR:FILEPATH=%LIBRARY_PREFIX%" ^
       -D MEDFILE_INSTALL_DOC=OFF ^
       -D MEDFILE_BUILD_PYTHON=ON ^
-      -D "PYTHON_EXECUTABLE:FILEPATH=%PYTHON%" ^
       -D "PYTHON_LIBRARY:FILEPATH=%PREFIX%\libs\python%MY_PY_VER%.lib" ^
       -D "PYTHON_INCLUDE_DIR:FILEPATH=%PREFIX%\include" -S . -B build
 
@@ -20,6 +20,7 @@ cmake --install .\build
 DEL /q %PREFIX%\bin\mdump %PREFIX%\bin\xmdump
 COPY /y %PREFIX%\bin\mdump4 %PREFIX%\bin\mdump
 COPY /y %PREFIX%\bin\xmdump4 %PREFIX%\bin\xmdump
+XCOPY /s /e /y %PREFIX%\lib\python%PY_VER%\site-packages\med %SP_DIR%
 
 
 REM ninja test
