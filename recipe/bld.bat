1adb0ac0c0b34be7eb01fb2c0d67592d4cdf7f41
Luca Dall'Olio <luca.dallolio@aego.ai>
2023-03-12 18:33:01 +0100

    rerendered, pinning, switching to move instead of xcopy

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,5 +1,3 @@
-REM Remove dot from PY_VER for use in library name
-set MY_PY_VER=%PY_VER:.=%
 
 cmake -G "Ninja" ^
       -Wno-dev ^
@@ -9,7 +7,7 @@ cmake -G "Ninja" ^
       -D "HDF5_ROOT_DIR:FILEPATH=%LIBRARY_PREFIX%" ^
       -D MEDFILE_INSTALL_DOC=OFF ^
       -D MEDFILE_BUILD_PYTHON=ON ^
-      -D "PYTHON_LIBRARY:FILEPATH=%PREFIX%\libs\python%MY_PY_VER%.lib" ^
+      -D "PYTHON_LIBRARY:FILEPATH=%PREFIX%\libs\python%CONDA_PY%.lib" ^
       -D "PYTHON_INCLUDE_DIR:FILEPATH=%PREFIX%\include" -S . -B build
 
 if errorlevel 1 exit 1
@@ -21,8 +19,8 @@ DEL /q %PREFIX%\bin\mdump %PREFIX%\bin\xmdump
 COPY /y %PREFIX%\bin\mdump4 %PREFIX%\bin\mdump
 COPY /y %PREFIX%\bin\xmdump4 %PREFIX%\bin\xmdump
 MKDIR %SP_DIR%\med
-XCOPY /s /e /y %PREFIX%\lib\python%PY_VER%\site-packages\med %SP_DIR%\med
-XCOPY /s /e /y %PREFIX%\lib\medC.* %LIBRARY_BIN%
+MOVE %PREFIX%\lib\python%PY_VER%\site-packages\med %SP_DIR%\med
+MOVE %PREFIX%\lib\medC.* %LIBRARY_BIN%
 
 
 REM ninja test
